b61862b2bdbd00003e671c4376a9153812780601
Nando <nandodavila94@gmail.com>
2021-11-21 15:59:51 -0500

    defined colomns in models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_on_this_product'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_with_this_tag'
+});
 
 module.exports = {
   Product,

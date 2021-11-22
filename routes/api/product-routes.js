2e3edf282a3ddb9caa8fc58a3862fa59ee59099c
Nando <nandodavila94@gmail.com>
2021-11-21 19:02:44 -0500

    maybe done with product routes?

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category },{ model: Tag, as: 'tags_on_this_product' }]
@@ -105,7 +105,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product with that ID!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
